3dc0592771ab0b8bec423b4468204b3e024a11a3
Betsy <132311670+betsymao@users.noreply.github.com>
2024-02-08 22:09:30 +1100

    update config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,23 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(( { mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+    plugins: [react()],
+    server: {
+      port: env.VITE_PORT,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+        }
+      }
+    }
+  }
 });
