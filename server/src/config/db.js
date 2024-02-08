4a1cf58af91a4eef6e11cfbc793db3f6132d6642
Betsy <132311670+betsymao@users.noreply.github.com>
2024-02-08 21:45:48 +1100

    added preview

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -7,9 +7,25 @@ try {
   dbStartup('Attempting database connection.');
   // set up database credentials and options
   let serviceAccountKey;
-  // standard setup: env key
+  // standard setup: env key; recommended setup
   if (config.env === 'development' || config.env === 'production') {
     serviceAccountKey = config.db.google_account_credentials;
+
+  // new setup: separate envs
+  } else if (config.env === 'preview') {
+    serviceAccountKey = {
+      type: config.db.type,
+      project_id: config.db.project_id,
+      private_key_id: config.db.private_key_id,
+      private_key: config.db.private_key,
+      client_email: config.db.client_email,
+      client_id: config.db.client_id,
+      auth_uri: config.db.auth_uri,
+      token_uri: config.db.token_uri,
+      auth_provider_x509_cert_url: config.db.client_x509_cert_url,
+      client_x509_cert_url: config.db.client_x509_cert_url,
+      universe_domain: config.db.universe_domain,     
+    }
   }
   dbStartup(serviceAccountKey);
 
